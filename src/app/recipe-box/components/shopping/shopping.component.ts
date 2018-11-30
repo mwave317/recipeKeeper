04c2568586b27e884988029a614dcf4cb6cd3f64
Ken <mwave317@gmail.com>
2018-11-30 10:01:07 -0500

    Added the ingredients for the shopping list.

--- src/app/recipe-box/components/shopping/shopping.component.ts
+++ src/app/recipe-box/components/shopping/shopping.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../../models/ingredient.model';
 
 @Component({
   selector: 'app-shopping',
@@ -6,7 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping.component.css']
 })
 export class ShoppingComponent implements OnInit {
-  ingredients: any[] = [];
+  ingredients: Ingredient[] = [
+    new Ingredient('Chicken Breasts', '2lbs.'),
+    new Ingredient('Tomato Sauce', '1.5 cups'),
+    new Ingredient('Mozzeralla Cheese', '4 thick cut slices'),
+    new Ingredient('Vegetable Oil', '1/2 cup'),
+    new Ingredient('Italian Bread Crumbs', '2 cups'),
+    new Ingredient('Large Eggs', '6'),
+  ];
+
   constructor() { }
 
   ngOnInit() {
